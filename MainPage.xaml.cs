f5dc24351e3e1d2fc4eb44972f5825510bafc8fc
gabsouza-dev <dev.gbrlsouza@gmail.com>
2025-01-27 11:31:46 -0300

    Create MainPage.xaml.cs

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using Xamarin.Forms;
+
+namespace MyFirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            label.Text = "Hello, Xamarin!";
+        }
+    }
+}
